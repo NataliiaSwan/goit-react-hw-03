1a0ae753793e1d83823e3638ac359f094865027f
NataliiaSwan <148957684+NataliiaSwan@users.noreply.github.com>
2024-05-29 16:44:03 +0300

    Add Components

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component, useState, useEffect } from "react";
+
+import "./App.css";
+
+import ContactList from "./ContactList/ContactList";
+
+import SearchBox from "./SearchBox/SearchBox";
+
+import ContactForm from "./ContactForm/ContactForm";
+
+const App = () => {
+  const initialContacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [contacts, setContacts] = useState((initialContacts) => {
+    const storedContacts = JSON.parse(localStorage.getItem("contacts"));
+    return storedContacts || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (contact) => {
+    setContacts([...contacts, contact]);
+  };
+
+  const handleSearch = (value) => {
+    setSearchTerm(value);
+  };
+
+  const handleDeleteContact = (contactId) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactId)
+    );
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1 className="title">Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <SearchBox searchTerm={searchTerm} onSearchChange={handleSearch} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
+
+export default App;
